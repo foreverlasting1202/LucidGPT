7f800e15b73dda87a234d4fdb5528dca851b7a6d
foreverlasting1202 <foreverlasting1202@outlook.com>
2026-03-06 16:33:51 +0800

    update README.md

--- models/nanoGPT.py
+++ models/nanoGPT.py
@@ -6,8 +6,6 @@ import math
 import torch.nn.functional as F
 from dataclasses import dataclass
 
-# -----------------------------------------------------------------------------
-# PyTorch nn.Module definitions for the GPT-2 model
 
 class Rotary(torch.nn.Module):
 
@@ -50,7 +48,7 @@ class CausalSelfAttention(nn.Module):
         self.c_v = nn.Linear(self.n_embd, self.n_embd, bias=False)
         # output projection
         self.c_proj = nn.Linear(self.n_embd, self.n_embd, bias=False)
-        self.c_proj.weight.data.zero_() # zero init suggested by @Grad62304977
+        self.c_proj.weight.data.zero_() # zero init
         self.rotary = Rotary(self.head_dim)
 
     def forward(self, x):
@@ -59,7 +57,7 @@ class CausalSelfAttention(nn.Module):
         k = self.c_k(x).view(B, T, self.n_head, self.head_dim)
         v = self.c_v(x).view(B, T, self.n_head, self.head_dim)
         cos, sin = self.rotary(q)
-        q, k = F.rms_norm(q, (q.size(-1),)), F.rms_norm(k, (k.size(-1),)) # QK norm suggested by @Grad62304977
+        q, k = F.rms_norm(q, (q.size(-1),)), F.rms_norm(k, (k.size(-1),)) # QK norm
         q, k = apply_rotary_emb(q, cos, sin), apply_rotary_emb(k, cos, sin)
         y = F.scaled_dot_product_attention(q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2), is_causal=True)
         y = y.transpose(1, 2).contiguous().view_as(x) # re-assemble all head outputs side by side
@@ -72,7 +70,7 @@ class MLP(nn.Module):
         super().__init__()
         self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=False)
         self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=False)
-        self.c_proj.weight.data.zero_() # zero init suggested by @Grad62304977
+        self.c_proj.weight.data.zero_() # zero init
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -92,14 +90,11 @@ class Block(nn.Module):
         x = x + self.mlp(F.rms_norm(x, (x.size(-1),)))
         return x
 
-# -----------------------------------------------------------------------------
-# The main GPT-2 model
-
 @dataclass
 class GPTConfig:
     vocab_size : int = 50304
     n_layer : int = 12
-    n_head : int = 6 # head dim 128 suggested by @Grad62304977
+    n_head : int = 6 # head dim 128
     n_embd : int = 768
 
 class GPT(nn.Module):
@@ -111,10 +106,8 @@ class GPT(nn.Module):
         self.transformer = nn.ModuleDict(dict(
             wte = nn.Embedding(config.vocab_size, config.n_embd),
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
-            # rotate = nn.Linear(config.n_embd, config.n_embd, bias=False), 
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
-        # self.transformer.wte.weight = self.lm_head.weight # https://paperswithcode.com/method/weight-tying
 
     def forward(self, idx, targets=None, return_logits=True):
         # forward the GPT model itself
@@ -122,7 +115,6 @@ class GPT(nn.Module):
         for block in self.transformer.h:
             x = block(x)
         x = F.rms_norm(x, (x.size(-1),))
-        # x = self.transformer.rotate(F.rms_norm(x, (x.size(-1),)))
         if targets is not None:
             # if we are given some desired targets also calculate the loss
             logits = self.lm_head(x)
